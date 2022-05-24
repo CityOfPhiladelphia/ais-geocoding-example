b3924d18f64b7650fb7cddc9dac6ea76fed5dd01
Victor Espinoza <vespinoza0391@gmail.com>
2022-05-24 16:46:39 -0400

    Get coordinates from address summary table.

--- new_script.py
+++ new_script.py
@@ -1,106 +1,135 @@
-import csv
-from config import aisCredentials
+import petl as etl
+import geopetl
+from config import aisCredentials, source_creds
+from passyunk.parser import PassyunkParser
 import requests
+import cx_Oracle
+import datetime as dt
 
-def ais_request(address_string):
+# request AIS for X and Y coordinates
+def ais_request(address_string,srid):
+    '''
+    :param address_string:
+    :param srid:
+    :return: list containing X and Y coordinates
+    '''
     ais_url = aisCredentials['url']
     params = {'gatekeeperKey': aisCredentials['gatekeeperKey']}
     request = "{ais_url}{geocode_field}".format(ais_url=ais_url, geocode_field=address_string)
-    # send request to tomtom
+    request = request+'?srid='+srid
     try:
         r = requests.get(request, params=params)
-        print('ais request for ', address_string)
-        print(r)
+        print('ais request response')
+        if r.status_code ==404 :
+            print('404 error')
+            raise
     except Exception as e:
-        logging.ERROR("Failed AIS request")
+        print("Failed AIS request")
         raise e
-
     # extract coordinates from json request response
     feats = r.json()['features'][0]
     geo = feats.get('geometry')
     coords = geo.get('coordinates')
-    coords = [str(c) for c in coords]
-    coords = ' '.join(coords)
-    opanum = feats.get('opa_account_num')
     return coords
 
 
-# return tomtom first candidate address
-def tomtom_request(street_str):
-    s = street_address.split(' ')
+# request tomtom for X and Y coordinates
+def tomtom_request(street_str,srid):
+    '''
+    :param street_str: string
+    :param srid:
+    :return: list containing X and Y coordinates
+    '''
+    s = street_str.split(' ')
     address = '+'.join(s)
     request_str = '''https://citygeo-geocoder-aws.phila.city/arcgis/rest/services/TomTom/US_StreetAddress/GeocodeServer/findAddressCandidates?Street={}
                 &City=&State=&ZIP=&Single+Line+Input=&outFields=&maxLocations=&matchOutOfRange=true&langCode=&locationType=&sourceCountry=&category=
-                &location=&distance=&searchExtent=&outSR=&magicKey=&f=pjson'''.format(address)
+                &location=&distance=&searchExtent=&outSR={}&magicKey=&f=pjson'''.format(address,srid)
     # send request to tomtom
     try:
         r = requests.get(request_str)
-        print('got tomtom request ')
     except Exception as e:
         print("Failed tomtom request")
         raise e
     # try to get a top address candidate if any
     try:
         top_candidate =  r.json().get('candidates')[0].get('location')
-        top_candidate = '{} {}'.format(str(top_candidate.get('x')), str(top_candidate.get('y')))
+        top_candidate = [top_candidate.get('x') ,top_candidate.get('y')]
     except:
-        return 'tom tom unable to geocode'
+        print('failed to geocode ', street_str)
+        print('\n\n')
+        return ['NA','NA']
     return top_candidate
 
-## NEW LOGIC
-# if city column
-#   if citycol_val == phila, philadelphia etc
-        # send to ais
-    #else:
-        #send to tomtom
-#else:
-    #if 'phil or philadedlephia etc' in streetadress val :
-        #send to ais
-    # else
-        #try:
-            # send to ais
-        #except:
-            # send to tomtom
 
-i =0
-with open('ais_geocoding_example_input.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    new_rows = []
-    for row in csv_reader:
-        if i == 0:
-            # add coordinates to new header
-            row.append('coordinates')
-            new_rows.append(row)
-        else: # for every row except header
-            street_address =row[1]
-            city_col =row[2]
+# conect to source table
+target_dsn = cx_Oracle.makedsn(source_creds.get('dsn').get('host'),
+                               source_creds.get('dsn').get('port'),
+                               service_name=source_creds.get('dsn').get('service_name'))
+target_conn = cx_Oracle.connect(source_creds.get('username'), source_creds.get('password'), target_dsn)
+target_cursor = target_conn.cursor()
 
-            if city_col:
-                # if philadelphia in city val use ais else try tomtom
-                if city_col== 'philadelphia':
-                    geocoded = ais_request(street_address)
-                else: # city is not philly
-                    print('tomtom get {} {}'.format(street_address,city_col))
-                    geocoded = tomtom_request(street_address)
-                    print('tomtom geocoded ', geocoded)
+# # address summary table fields
+# if geocode_srid == 2272:
+#     adrsum_fields = ['street_address', 'geocode_x', 'geocode_y']
+# else:
+#     adrsum_fields = ['street_address', 'geocode_lon', 'geocode_lat']
+adrsum_fields = ['street_address', 'geocode_x', 'geocode_y', 'geocode_lon', 'geocode_lat']
 
-            else:# if no city column
-                # if philadelphia in address use ais else try tomtom
-                if 'philadelphia' in street_address:
-                    geocoded = ais_request(street_address)
-                else:
-                    try:
-                        geocoded = ais_request(street_address)
-                    except:
-                        geocoded= tomtom_request(street_address)
+# extract data from source table
+address_summary_rows = etl.fromoraclesde(target_conn, 'ADDRESS_SUMMARY', fields=adrsum_fields)
+#address_summary_rows = etl.fromcsv('address_summary_{}.csv'.format(geocode_srid))
 
-            #this_list.append(geocoded)
-            row.append(geocoded)
-            new_rows.append(row)
-        i=i+1
+parser = PassyunkParser()
+#input address data from input csv
+input_address = etl.fromcsv('ais_geocoding_example_input.csv')#[adrsum_fields]
+# add standardized address column to input csv
+input_address = input_address.addfield('addr_std', lambda p: parser.parse(p.street_address)['components']['output_address'])
 
-# write output in csv file
-with open("out_test.csv", "w", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerows(new_rows)
+#join input data with source table data
+joined_addresses_to_address_summary = etl.leftjoin(input_address, address_summary_rows, lkey='addr_std', rkey='street_address', presorted=False )
+joined_addresses_to_address_summary.tocsv('test_joined_addresses_unsorted_{}.csv'.format(geocode_srid))
+# joined_addresses_to_address_summary = etl.fromcsv('test_joined_addresses_unsorted_{}.csv'.format(geocode_srid))
+
+#joined_table header
+header = list(etl.fieldnames(joined_addresses_to_address_summary))
+# modify header
+if 'geocode_x' in header:
+    header[header.index('geocode_x')] = 'x'
+elif 'geocode_lon' in header:
+    header[header.index('geocode_lon')] = 'x'
+if 'geocode_y' in header:
+    header[header.index('geocode_y')] = 'y'
+elif 'geocode_lat' in header:
+    header[header.index('geocode_lat')] = 'y'
+
+newlist = []
+for row in joined_addresses_to_address_summary[1:]:
+    rowzip = dict(zip(header, row))  # dictionary from etl data
+    #if there is a longitude coordinates from address summary continue
+    if rowzip.get('geocode_lon'):
+        newlist.append(rowzip)
+        continue
+    elif rowzip.get('city'):
+        srid = geocode_srid#rowzip.get('srid')
+        if rowzip.get('city') == 'philadelphia':
+            geocoded = ais_request(rowzip.get('street_address'),str(srid))
+        else:  # city is not philly
+            geocoded = tomtom_request(rowzip.get('street_address'),str(srid))
+    else:  # if no city column
+        # if philadelphia in address use ais else try tomtom
+        if 'philadelphia' in rowzip.get('street_address'):
+            geocoded = ais_request(rowzip.get('street_address'),str(srid))
+        else:
+            try:
+                geocoded = ais_request(rowzip.get('street_address'),str(srid))
+                print('AIS geocoded ', geocoded)
+            except:
+                geocoded = tomtom_request(rowzip.get('street_address'),str(srid))
+    rowzip['x'] = geocoded[0]
+    rowzip['y'] = geocoded[1]
+    newlist.append(rowzip)
+
+newframe = etl.fromdicts(newlist,header=header)
+newframe.tocsv('geocoded_output.csv')
+
