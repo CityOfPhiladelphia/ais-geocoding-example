dc9e01e31c972b740e097a252e800705d9ac91e1
vespinoza0 <vespinoza0391@gmail.com>
2023-01-24 18:01:04 -0500

    Update logic with zipcode info.

--- geocode_example.py
+++ geocode_example.py
@@ -73,22 +73,22 @@ def tomtom_request(street_str,srid):
 def main():
 
     # download address summary using requests
-    response = requests.get('https://opendata-downloads.s3.amazonaws.com/address_summary.csv')
-    lines = response.text.splitlines()
-    mydata = [s.split(',') for s in lines]
-    # save address summary table to memory
-    with open('address_summary_fields.csv', 'w', encoding='UTF8', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerows(mydata)
+    # response = requests.get('https://opendata-downloads.s3.amazonaws.com/address_summary.csv')
+    # lines = response.text.splitlines()
+    # mydata = [s.split(',') for s in lines]
+    # # save address summary table to memory
+    # with open('address_summary_fields.csv', 'w', encoding='UTF8', newline='') as f:
+    #     writer = csv.writer(f)
+    #     writer.writerows(mydata)
     # required address_summary (source) table fields
     if geocode_srid == 2272:
-        adrsum_fields = ['street_address', 'geocode_x', 'geocode_y']
+        adrsum_fields = ['street_address', 'geocode_x', 'geocode_y','zip_code']
         # load address summary data from csv to petl frame
         address_summary_rows = etl.fromcsv('address_summary_fields.csv').cut(adrsum_fields)
         address_summary_rows = etl.rename(address_summary_rows,
                                           {'geocode_x': 'x_coordinate', 'geocode_y': 'y_coordinate'})
     elif geocode_srid == 4326:
-        adrsum_fields = ['street_address', 'geocode_lon', 'geocode_lat']
+        adrsum_fields = ['street_address', 'geocode_lon', 'geocode_lat','zip_code']
         # load address summary data from csv to petl frame
         address_summary_rows = etl.fromcsv('address_summary_fields.csv').cut(adrsum_fields)
         address_summary_rows = etl.rename(address_summary_rows,
@@ -96,42 +96,61 @@ def main():
     else:
         print('invalid srid')
         raise
+    cols = etl.columns(address_summary_rows)
+    z = cols['zip_code']
+    philly_zipcodes = list(set(z))
+    philly_zipcodes.remove('')
+    philly_zipcodes = [int(z) for z in philly_zipcodes]
 
     # get input address test data from input csv
     input_addresses = etl.fromcsv('ais_geocoding_example_input.csv')
-
+    cityresult=0
+    stateresult=0
     #passayunk instance
     parser = PassyunkParser()
     dict_frame = []
     # try to get state and city using open source parser
     for row in input_addresses[1:]:
         mydict = dict(zip(input_addresses[0],row))
-        # try to get state info using open source parser
-        if not mydict.get('state'):
-            res = parse_location(mydict.get('street_address'))
-            if res.get('state'):
-                mydict['state'] = res.get('state')
-        # try to get city info using open source parser
-        if not mydict.get('city') or mydict.get('city').lower() != 'philadelphia':
-            res = parse_location(mydict['street_address'])
-            if res.get('city'):
-                mydict['city'] = res.get('city')
-        # if city is philly use passayunk to standardize street address
-        if mydict.get('city').lower() == 'philadelphia':
-            d = parser.parse(mydict.get('street_address'))
-            mydict['address_std']=d['components']['output_address']
-        else:
-            # leave std address as none???
-            mydict['address_std'] = None
+        ###################################################
+        # if city is philly:
+        #     use passayunk
+        # elif not city:
+        #     use oparser
+        #     if zip is in philly or city is philly:
+        ###################################################
+
+        if mydict.get('city') and mydict.get('city').lower() == 'philadelphia':
+            std_address = parser.parse(mydict.get('street_address'))
+            mydict['std_address'] = std_address['components']['output_address']
+        if not mydict.get('city'): # if no city input
+            parser_response = parse_location(mydict.get('street_address'))
+            if (parser_response and parser_response.get('zip') and parser_response.get('zip') in philly_zipcodes)\
+                or (parser_response and parser_response.get('city') and parser_response.get('city').lower() == 'philadelphia'):
+                std_address = parser.parse(mydict.get('street_address'))
+                mydict['std_address'] = std_address
+            else:# fill in city or state which seems unecessary?
+                mydict['std_address'] = None
+                if parser_response and parser_response.get('city') and not mydict.get('city'):
+                    mydict['city'] =parser_response.get('city')
+                    cityresult = cityresult + 1
+
+                if parser_response and parser_response.get('state') and not mydict.get('state'):
+                    mydict['state'] =parser_response.get('state')
+                    stateresult = stateresult + 1
+
         dict_frame.append(mydict)
     # input addresses with std_address field
-    header = list(input_addresses[0]).append('address_std')
+    header = list(input_addresses[0]).append('std_address')
     input_addresses = etl.fromdicts(dict_frame, header=header)
 
     #join input data with address summary table data on standardized street address column
-    joined_addresses_to_address_summary = etl.leftjoin(input_addresses, address_summary_rows, lkey='address_std', rkey='street_address', presorted=False )
+    joined_addresses_to_address_summary = etl.leftjoin(input_addresses, address_summary_rows, lkey='std_address', rkey='street_address', presorted=False )
+
 
     dict_frame = []
+
+    # use apis to get coordinates
     for row in joined_addresses_to_address_summary[1:]:
         rowzip = dict(zip(joined_addresses_to_address_summary[0], row))
         # address already has coordinates from join
@@ -140,6 +159,7 @@ def main():
             continue
         # address does not have coordinates from join
         else:
+            geocoded = None
             # if we have standardized address use them for apis else
             if rowzip.get('address_std'):
                 try:
@@ -148,6 +168,7 @@ def main():
                     geocoded = tomtom_request(rowzip.get('address_std'), str(geocode_srid))
             # else use street address input field
             else:
+                print('non std address ', rowzip.get('street_address'),)
                 try:
                     geocoded = ais_request(rowzip.get('street_address'), str(geocode_srid))
                 except:
